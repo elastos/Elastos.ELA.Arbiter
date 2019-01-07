d7e6ce9cfd85929c95ce4fa9ca79992e0a49d940
Jiang ZeHua <1092431698@qq.com>
2019-01-07 15:03:48 +0800

    remove blockInfo to block function and modify side chain mining (#296)
    remove blockInfo to block function and modify side chain mining

--- arbitration/arbitrator/auxpowlistener.go
+++ arbitration/arbitrator/auxpowlistener.go
@@ -1,12 +1,18 @@
 package arbitrator
 
 import (
+	"bytes"
+
+	"github.com/elastos/Elastos.ELA.Arbiter/config"
 	"github.com/elastos/Elastos.ELA.Arbiter/log"
 
 	"github.com/elastos/Elastos.ELA.SPV/bloom"
 	spv "github.com/elastos/Elastos.ELA.SPV/interface"
+	"github.com/elastos/Elastos.ELA.SPV/interface/iutil"
 	"github.com/elastos/Elastos.ELA/common"
 	"github.com/elastos/Elastos.ELA/core/types"
+	"github.com/elastos/Elastos.ELA/core/types/payload"
+	"github.com/elastos/Elastos.ELA/p2p/msg"
 )
 
 type AuxpowListener struct {
@@ -39,115 +45,120 @@ func (l *AuxpowListener) Notify(id common.Uint256, proof bloom.MerkleProof, tx t
 }
 
 func (l *AuxpowListener) ProcessNotifyData(tasks []*notifyTask) {
-	// fixme uncommon after sidechain auxpow moved to ELA.Sidechain
+	task := tasks[len(tasks)-1]
+	log.Info("[Notify-ProcessNotifyData][", l.ListenAddress, "] process hash:", task.tx.Hash().String(), "len tasks:", len(tasks))
+	err := SpvService.VerifyTransaction(*task.proof, *task.tx)
+	if err != nil {
+		log.Error("verify transaction error: ", err)
+		return
+	}
 
-	//l.lock.Lock()
-	//for _, t := range tasks {
-	//	delete(l.msgs, t.tx.Hash())
-	//}
-	//l.lock.Unlock()
-	//
-	//task := tasks[len(tasks)-1]
-	//log.Info("[Notify-ProcessNotifyData][", l.ListenAddress, "] process hash:", task.tx.Hash().String(), "len tasks:", len(tasks))
-	//err := SpvService.VerifyTransaction(*task.proof, *task.tx)
-	//if err != nil {
-	//	log.Error("Verify transaction error: ", err)
-	//	return
-	//}
-	//
-	//// Get Header from main chain
-	//header, err := SpvService.HeaderStore().Get(&task.proof.BlockHash)
-	//if err != nil {
-	//	log.Error("can not get block from main chain")
-	//	return
-	//}
-	//
-	//// Check if merkleroot is match
-	//merkleBlock := msg.MerkleBlock{
-	//	Header:       header.BlockHeader,
-	//	Transactions: task.proof.Transactions,
-	//	Hashes:       task.proof.Hashes,
-	//	Flags:        task.proof.Flags,
-	//}
-	//
-	//txId := task.tx.Hash()
-	//merkleBranch, err := bloom.GetTxMerkleBranch(merkleBlock, &txId)
-	//if err != nil {
-	//	log.Error("can not get merkle branch")
-	//	return
-	//}
-	//
-	//elaHeader := header.BlockHeader.(*iutil.Header)
-	//// sideAuxpow serilze
-	//sideAuxpow := auxpow.SideAuxPow{
-	//	SideAuxMerkleBranch: merkleBranch.Branches,
-	//	SideAuxMerkleIndex:  merkleBranch.Index,
-	//	SideAuxBlockTx:      *task.tx,
-	//	MainBlockHeader:     *elaHeader.Header,
-	//}
-	//
-	//sideAuxpowBuf := bytes.NewBuffer([]byte{})
-	//err = sideAuxpow.Serialize(sideAuxpowBuf)
-	//if err != nil {
-	//	log.Error("SideAuxpow serialize error: ", err)
-	//	return
-	//}
-	//
-	//// send submit block
-	//payload, ok := task.tx.Payload.(*types.PayloadSideChainPow)
-	//if !ok {
-	//	log.Error("Invalid payload type.")
-	//	return
-	//}
-	//blockhashString := payload.SideBlockHash.String()
-	//genesishashString := payload.SideGenesisHash.String()
-	//blockHeight := payload.BlockHeight
-	//
-	//sideAuxpowData := sideAuxpowBuf.Bytes()
-	//sideAuxpowString := common.BytesToHexString(sideAuxpowData)
-	//
-	//var sideChain SideChain
-	//for _, sideNode := range config.Parameters.SideNodeList {
-	//	log.Info("Side node genesis block:", sideNode.GenesisBlock,
-	//		"side aux pow tx genesis hash:", genesishashString)
-	//	if sideNode.GenesisBlock == genesishashString {
-	//		sc, ok := ArbitratorGroupSingleton.GetCurrentArbitrator().
-	//			GetSideChainManager().GetChain(sideNode.GenesisBlockAddress)
-	//		if ok {
-	//			currentHeight, err := sc.GetCurrentHeight()
-	//			if err != nil {
-	//				log.Error("Side chain GetCurrentHeight failed")
-	//				return
-	//			}
-	//			if currentHeight == blockHeight {
-	//				sideChain = sc
-	//			} else {
-	//				log.Warn("No need to submit auxpow, current side chain height:",
-	//					currentHeight, " block height:", blockHeight)
-	//				return
-	//			}
-	//		}
-	//	}
-	//}
-	//
-	//if sideChain == nil {
-	//	log.Error("Arbiter not find side chain")
-	//	allChains := ArbitratorGroupSingleton.GetCurrentArbitrator().GetSideChainManager().GetAllChains()
-	//	for index, chain := range allChains {
-	//		log.Error("Side chain", index, ":", chain.GetKey())
-	//	}
-	//
-	//	log.Error("Can not find side chain from genesis block hash: [", genesishashString, "]")
-	//	return
-	//}
-	//
-	//sideChain.UpdateLastNotifySideMiningHeight(payload.SideGenesisHash)
-	//err = sideChain.SubmitAuxpow(genesishashString, blockhashString, sideAuxpowString)
-	//if err != nil {
-	//	log.Error("[Notify-Auxpow] Submit SideAuxpow error: ", err)
-	//	return
-	//}
-	//sideChain.UpdateLastSubmitAuxpowHeight(payload.SideGenesisHash)
+	// Get Header from main chain
+	header, err := SpvService.HeaderStore().Get(&task.proof.BlockHash)
+	if err != nil {
+		log.Error("can not get block from main chain")
+		return
+	}
+
+	// Check if merkleroot is match
+	merkleBlock := msg.MerkleBlock{
+		Header:       header.BlockHeader,
+		Transactions: task.proof.Transactions,
+		Hashes:       task.proof.Hashes,
+		Flags:        task.proof.Flags,
+	}
+
+	txId := task.tx.Hash()
+	merkleBranch, err := bloom.GetTxMerkleBranch(merkleBlock, &txId)
+	if err != nil {
+		log.Error("can not get merkle branch")
+		return
+	}
+
+	// serialize main chain tx
+	buf := new(bytes.Buffer)
+	if err := task.tx.Serialize(buf); err != nil {
+		log.Error("invalid payload tx")
+		return
+	}
+
+	// serialize merkle branch
+	if err := common.WriteUint32(buf, uint32(len(merkleBranch.Branches))); err != nil {
+		log.Error("serialize merkle branch count failed:", err.Error())
+		return
+	}
+	for _, branch := range merkleBranch.Branches {
+		err = branch.Serialize(buf)
+		if err != nil {
+			log.Error("serialize merkle branch failed:", err.Error())
+			return
+		}
+	}
+	if err := common.WriteUint32(buf, uint32(merkleBranch.Index)); err != nil {
+		log.Error("serialize merkle branch index failed:", err.Error())
+		return
+	}
+
+	// serialize ela header
+	elaHeader := header.BlockHeader.(*iutil.Header)
+	if err := elaHeader.Serialize(buf); err != nil {
+		log.Error("invalid elaHeader")
+		return
+	}
+
+	sideAuxpowString := common.BytesToHexString(buf.Bytes())
+
+	p, ok := task.tx.Payload.(*payload.PayloadSideChainPow)
+	if !ok {
+		log.Error("invalid payload type")
+		return
+	}
+	blockhashString := p.SideBlockHash.String()
+	genesishashString := p.SideGenesisHash.String()
+	blockHeight := p.BlockHeight
+
+	var sideChain SideChain
+	for _, sideNode := range config.Parameters.SideNodeList {
+		log.Info("side node genesis block:", sideNode.GenesisBlock,
+			"side aux pow tx genesis hash:", genesishashString)
+		if sideNode.GenesisBlock == genesishashString {
+			sc, ok := ArbitratorGroupSingleton.GetCurrentArbitrator().
+				GetSideChainManager().GetChain(sideNode.GenesisBlockAddress)
+			if ok {
+				currentHeight, err := sc.GetCurrentHeight()
+				if err != nil {
+					log.Error("side chain GetCurrentHeight failed")
+					return
+				}
+				if currentHeight == blockHeight {
+					sideChain = sc
+				} else {
+					log.Warn("No need to submit auxpow, current side chain height:",
+						currentHeight, " block height:", blockHeight)
+					return
+				}
+			}
+		}
+	}
+
+	if sideChain == nil {
+		log.Error("arbiter not find side chain")
+		allChains := ArbitratorGroupSingleton.GetCurrentArbitrator().GetSideChainManager().GetAllChains()
+		for index, chain := range allChains {
+			log.Error("side chain", index, ":", chain.GetKey())
+		}
+
+		log.Error("can not find side chain from genesis block hash: [", genesishashString, "]")
+		return
+	}
+
+	sideChain.UpdateLastNotifySideMiningHeight(p.SideGenesisHash)
+	err = sideChain.SubmitAuxpow(genesishashString, blockhashString, sideAuxpowString)
+	if err != nil {
+		log.Error("[Notify-Auxpow] submit SideAuxpow error: ", err)
+		return
+	}
+	sideChain.UpdateLastSubmitAuxpowHeight(p.SideGenesisHash)
 }
 
 func (l *AuxpowListener) start() {
