5da640ba9f93ca9cf5e3c87faae224677f21550c
Houshoupei84 <houshoupei@126.com>
2023-09-05 18:48:44 +0800

    fix(bpos): SendCachedNFTDestroyTxs add RemoveNFTDestroyTxs

--- rpc/jsonrpc.go
+++ rpc/jsonrpc.go
@@ -362,7 +362,7 @@ func GetCanNFTDestroyIDs(ids []string, GenesisBlockHash string) ([]string, error
 	parameter := make(map[string]interface{})
 	parameter["ids"] = ids
 	parameter["genesisblockhash"] = GenesisBlockHash
-	log.Infof(" [GetCanNFTDestroyIDs] ids ", ids, "genesisblockhash", GenesisBlockHash)
+	log.Info(" [GetCanNFTDestroyIDs] ids ", ids, "genesisblockhash", GenesisBlockHash)
 
 	result, err := CallAndUnmarshal("getcandestroynftids",
 		parameter, config.Parameters.MainNode.Rpc)
@@ -374,7 +374,7 @@ func GetCanNFTDestroyIDs(ids []string, GenesisBlockHash string) ([]string, error
 	if err := Unmarshal(&result, &canDestroyIDs); err != nil {
 		return nil, err
 	}
-	log.Infof("[GetCanNFTDestroyIDs]  canDestroyIDs ", canDestroyIDs)
+	log.Info("[GetCanNFTDestroyIDs]  canDestroyIDs ", canDestroyIDs)
 
 	return canDestroyIDs, nil
 }
